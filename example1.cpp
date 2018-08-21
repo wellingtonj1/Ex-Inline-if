c64f81c8377bfe677f0d7de196522bad8f93edb3
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-08-21 08:20:23 -0300

    Create example1.cpp

---
+++ example1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      int idade=30;      
+      
+      /*if(idade>=65)
+      {
+          puts("Voçê é velho");
+      }
+      else
+      {
+          puts("Voçễ é novo");
+      }
+      */
+      cout<<" Voçê é "<< (idade >=65 ? "velho" : "novo")<<endl;
+
+}
